59ee53eba1acb525b0ce3d735c7a877a8f12c5ae
James Seppi <james.seppi@gmail.com>
2016-06-23 08:34:44 -0500

    narrowed down issue to auth_complete

--- hourglass/uaa_backend.py
+++ hourglass/uaa_backend.py
@@ -1,8 +1,8 @@
-from social.backends.open_id import OpenIdConnectAuth
+from social.backends.open_id import OpenIdConnectAuth, OpenIdConnectAssociation
 from social.utils import handle_http_errors
 
 from hourglass.settings import (
-    UAA_OAUTH_AUTH_URL, UAA_OAUTH_TOKEN_URL,  # UAA_OAUTH_CALLBACK_URL
+    UAA_OAUTH_AUTH_URL, UAA_OAUTH_TOKEN_URL,
 )
 
 
@@ -17,13 +17,6 @@ class UAAOpenId(OpenIdConnectAuth):
     ACCESS_TOKEN_URL = UAA_OAUTH_TOKEN_URL  # 'http://localhost:3000/op/token'
     ACCESS_TOKEN_METHOD = 'POST'
 
-    # Override get_redirect_uri to return
-    # the config value UAA_OAUTH_CALLBACK_URL
-    # TODO: It seems like I shouldn't have to do this, but
-    # I can't find any documentation on how to better accomplish this
-    # def get_redirect_uri(self, state):
-    #     return UAA_OAUTH_CALLBACK_URL
-
     # TODO: Not sure if we need to implement this
     # def user_data(self, access_token, *args, **kwargs):
     #     """Loads user data from somewhere"""
@@ -31,26 +24,25 @@ class UAAOpenId(OpenIdConnectAuth):
 
     # TODO: Not sure if we need to implement this
     # def get_user_details(self, response):
-    #     print("-----------------------------")
-    #     print(response)
     #     return {
-    #         'email': 'whatever',
+    #         'email': 'something',
     #     }
 
-    # TODO: This needs to be implemented
-    # but it currently does not work
-    # saving the association model is failing
-    # https://github.com/omab/python-social-auth/blob/master/social/backends/open_id.py#L263
-    @handle_http_errors
-    def auth_complete(self, *args, **kwargs):
-        self.process_error(self.data)
-        response = self.request_access_token(
-            self.ACCESS_TOKEN_URL,
-            data=self.auth_complete_params(),
-            headers=self.auth_headers(),
-            method=self.ACCESS_TOKEN_METHOD
-        )
-        self.process_error(reponse)
-        return self.do_auth(response['access_token'],
-                            response=response,
-                            *args, **kwargs)
+    # @handle_http_errors
+    # def auth_complete(self, *args, **kwargs):
+    #     """Completes login process, must return user instance"""
+    #     state = self.validate_state()
+    #
+    #     # TODO: problem happens in the backends/oauth.py class here:
+    #     # 400 Client Error: Bad Request for url: http://localhost:3000/op/token
+    #     # It might be something as simple as having the wrong ACCESS_TOKEN_URL
+    #     response = self.request_access_token(
+    #         self.access_token_url(),
+    #         data=self.auth_complete_params(state),
+    #         headers=self.auth_headers(),
+    #         auth=self.auth_complete_credentials(),
+    #         method=self.ACCESS_TOKEN_METHOD
+    #     )
+    #     self.process_error(response)
+    #     return self.do_auth(response['access_token'], response=response,
+    #                         *args, **kwargs)
